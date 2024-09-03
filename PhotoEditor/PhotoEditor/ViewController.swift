e5f4d442fa61b535a255bccacce725b163e99477
DamirNur <119695906+DamirNur@users.noreply.github.com>
2024-09-03 23:14:58 +0300

    first commit

---
+++ PhotoEditor/PhotoEditor/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PhotoEditor
+//
+//  Created by Damir Nuriev on 3.09.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .red
+    }
+
+
+}
+
